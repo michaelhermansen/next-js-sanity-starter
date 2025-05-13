abf450e53a65b666d8b09c2ebcf38ab04d67ec9f
Michael Hermansen <michael.p.hermansen@gmail.com>
2025-05-13 19:27:24 +0200

    feat: cms-image component

---
+++ src/components/cms-image.tsx
@@ -0,0 +1,54 @@
+import { env } from "@/lib/env";
+import { DeepNullable } from "@/types/utils";
+import { SanityImage, SanityImageProps } from "sanity-image";
+import { shake } from "radash";
+
+type SimpleImageMetadata = DeepNullable<{
+  dimensions?: { width?: number; height?: number };
+}>;
+
+type SimpleImage = DeepNullable<{
+  asset:
+    | { _ref: string; _id?: string; metadata?: SimpleImageMetadata }
+    | { _ref?: string; _id: string; metadata?: SimpleImageMetadata };
+  hotspot?: { x?: number; y?: number };
+  crop?: { top?: number; left?: number; bottom?: number; right?: number };
+}>;
+
+export interface CmsImageProps
+  extends Omit<SanityImageProps<"img">, "id" | "hotspot" | "crop" | "baseUrl"> {
+  image: SimpleImage;
+  alt: string;
+}
+
+export function CmsImage(props: CmsImageProps) {
+  const { image, ...restProps } = props;
+
+  // Infer some props from image object
+  const id = image.asset?._ref || image.asset?._id;
+  const hotspot = { x: 0, y: 0, ...shake(image.hotspot) };
+  const crop = {
+    top: 0,
+    right: 0,
+    bottom: 0,
+    left: 0,
+    ...shake(image.crop),
+  };
+
+  if (!id) return null;
+
+  // Automatically set the baseUrl based on the current environment
+  const baseUrl = `https://cdn.sanity.io/images/${env.NEXT_PUBLIC_SANITY_PROJECT_ID}/${env.NEXT_PUBLIC_SANITY_DATASET}/`;
+
+  return (
+    <SanityImage
+      id={id}
+      hotspot={hotspot}
+      crop={crop}
+      baseUrl={baseUrl}
+      loading="lazy" // Set default to lazy loading
+      mode="cover" // Set default fitting mode to "cover"
+      {...restProps}
+    />
+  );
+}
