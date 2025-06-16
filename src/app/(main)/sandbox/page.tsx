40d4103fea90be6f4756a7b54f3a809337fe0a3a
Michael Hermansen <michael.p.hermansen@gmail.com>
2025-06-16 09:41:41 +0200

    feat: add card to sandbox

--- src/app/(main)/sandbox/page.tsx
+++ src/app/(main)/sandbox/page.tsx
@@ -1,18 +1,30 @@
 import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardLink } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 
 export default function Page() {
   return (
-    <div className="py-section container">
-      <form className="space-y-4">
-        <div>
-          <Label htmlFor="test">Test</Label>
-          <Input required id="test" />
-        </div>
+    <div>
+      <div className="py-section container border-b">
+        <form className="space-y-4">
+          <div>
+            <Label htmlFor="test">Test</Label>
+            <Input required id="test" />
+          </div>
 
-        <Button type="submit">Submit</Button>
-      </form>
+          <Button type="submit">Submit</Button>
+        </form>
+      </div>
+
+      <div className="py-section container border-b">
+        <Card clickable>
+          <CardContent>
+            <CardLink href="/">Hallo</CardLink>
+            <p>Test</p>
+          </CardContent>
+        </Card>
+      </div>
     </div>
   );
 }
